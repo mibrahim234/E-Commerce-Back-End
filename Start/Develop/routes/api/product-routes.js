fe34defa2ac392f669f73ee384a6b27fce2b7e91
mibrahim234 <mohamm1417@gmail.com>
2021-11-06 23:06:31 -0400

    post route

--- Start/Develop/routes/api/product-routes.js
+++ Start/Develop/routes/api/product-routes.js
@@ -59,15 +59,16 @@ router.get('/:id', async (req, res) => {
 
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/', async (req, res) => {
+ try {
+   const productData = await Product.create({
+     product_name: req.body.product_name,
+     price: req.body.price,
+     stock: req.body.stock,
+     category_id: req.body.category_id,
+     tagIds: req.body.tagIds
+   })
+ }
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
